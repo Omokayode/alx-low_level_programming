047783ac596bd2c3448eeca6f963c19d122e4a44
Omokayode John <Omokayode John>
2022-09-12 21:01:20 -0700

    auto push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,18 @@
 	#include <stdio.h>
 
 	/*
-	 * main - Entry point
- 	* 
+	* main - Entry point
+	* 	*
  	* Description: Print with proper grammar, but the outcome is a piece of art.
- 	*  
+	*   
 	* Return: Always o (Success)
 	*/
 	int main(void)
-	{	
-	     printf("with proper grammar, but the outcome is a piece of art,\n");
-	     return (0);
-	}
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
 
 
 
