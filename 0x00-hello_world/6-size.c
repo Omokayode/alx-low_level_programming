d7f41d84e98b4f16e89152709b4d71d9914a6dee
Omokayode John <Omokayode John>
2022-09-12 21:10:22 -0700

    auto push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
 /**
  *  * main - pint the size of various types
  *   *
- *    * Description: prints the size of various types on the computer it is compiled
+ *Description: prints the size of various types on the computer it is compiled
  *     *
  *      * and run on
  *       *
  *        * Return: Always 0 (Success)
- *         */
+ */
 
 int main(void)
 {
